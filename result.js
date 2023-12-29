c172eaac3bc45a2b274c24f377dcddc5763e499c
Claudia_Alves <claudiascarvalves@gmail.com>
2023-12-29 22:44:35 +0000

    logic to calculate bday added

--- result.js
+++ result.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const userName = localStorage.getItem('userName');
-  const dateofBirth = localStorage.getItem('dateOfBirth')
+  const dateOfBirth = localStorage.getItem('dateOfBirth')
 
   const resultName = document.getElementById('resultName');
-  resultName.textContent = `Happy Birthday, ${userName}!`
-
   const resultDOB = document.getElementById('resultDOB');
-  resultDOB.textContent = `Your birthday is ${dateofBirth}!`
+  const nextbday = document.getElementById('nextbday');
+
+  const todaysDate = new Date();
+  const birthday = new Date(dateOfBirth)
+
+  let nextBirthday = new Date(todaysDate.getFullYear(), birthday.getMonth(), birthday.getDate());
+
+  if (todaysDate > nextBirthday) {
+    nextBirthday.setFullYear(todaysDate.getFullYear() + 1);
+  }
+
+  const daysUntilNextBirthday = Math.ceil((nextBirthday - todaysDate) / (1000 * 60 * 60 * 24));
+
+  resultName.textContent = `Happy Birthday, ${userName}!`;
+  resultDOB.textContent = `Your birthday is on ${birthday.toDateString()}`;
+  nextbday.textContent= `Days until next birthday: ${daysUntilNextBirthday}`;
 })
