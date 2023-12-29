8c0db45a4ea6e5c2d66585558dacd8ee3f70be64
Claudia_Alves <claudiascarvalves@gmail.com>
2023-12-29 21:27:44 +0000

    added section for date of birth

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ document.getElementById('birthdayForm').addEventListener('submit', (event) => {
   event.preventDefault(); 
 
   const name = document.getElementById('name').value;
-  console.log('name saved ', name)
 
-  // const birthday = document.getElementById('birthday').value;
+  const birthday = document.getElementById('birthday').value;
 
   localStorage.setItem('userName', name);
-  // localStorage.setItem('dateOfBirth', birthday);
+  localStorage.setItem('dateOfBirth', birthday);
   
   window.location.href = 'result.html'
 
--- result.js
+++ result.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Retrieve the user name from local storage
+
   const userName = localStorage.getItem('userName');
-  console.log('Name retrieved ' + userName)
+  const dateofBirth = localStorage.getItem('dateOfBirth')
 
-  // Clear the user name from local storage (optional)
-  // localStorage.removeItem('userName');
+  const resultName = document.getElementById('resultName');
+  resultName.textContent = `Happy Birthday, ${userName}!`
 
-  // Example: Display a result message with the user name
-  // document.createElement('p')
-  const resultMessage = document.getElementById('resultMessage');
-  resultMessage.textContent = `Happy Birthday, ${userName}!`
+  const resultDOB = document.getElementById('resultDOB');
+  resultDOB.textContent = `Your birthday is ${dateofBirth}!`
 })
