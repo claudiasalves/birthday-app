472aa1480e4ea689e22b3231e0aa2f174e7437ec
Claudia_Alves <claudiascarvalves@gmail.com>
2024-01-08 12:19:38 +0000

    test to direct to result added

--- index.test.js
+++ index.test.js
@@ -5,7 +5,6 @@ const { handleFormSubmit } = require('./index');
 
 describe('handleFormSubmit', () => {
   it('redirects to bday.html for today\'s birthday', () => {
-
     document.body.innerHTML = `
       <form id="birthdayForm">
         <input id="name" type="text" value="John" />
@@ -21,7 +20,6 @@ describe('handleFormSubmit', () => {
       value: mockLocation,
       writable: true,
     });
-
     const originalLocationHref = window.location.href;
     window.location.href = '';
 
@@ -30,10 +28,37 @@ describe('handleFormSubmit', () => {
     });
 
     expect(window.location.href).toBe('bday.html');
-
     window.location.href = originalLocationHref;
   });
 
+
+
+  it('redirects to result.html for a birthday with day and month in the future', () => {
+    document.body.innerHTML = `
+      <form id="birthdayForm">
+        <input id="name" type="text" value="John" />
+        <input id="birthday" type="date" value="1990-12-31" />
+        <div id="nameError"></div>
+        <div id="birthdayError"></div>
+        <div id="loadingSpinner"></div>
+      </form>
+    `;
+
+    const mockLocation = { href: '' };
+    Object.defineProperty(window, 'location', {
+      value: mockLocation,
+      writable: true,
+    });
+    const originalLocationHref = window.location.href;
+    window.location.href = '';
+
+    handleFormSubmit({
+      preventDefault: jest.fn(),
+    });
+
+    expect(window.location.href).toBe('result.html');
+    window.location.href = originalLocationHref;
+  });
 
 });
   
